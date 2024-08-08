f214a29aa063045186194292432a55b2eb2bec4d
Polar4ik <atossnikitoss@gmail.com>
2024-08-08 14:09:29 +0400

    add translate to site

---
+++ main.js
@@ -0,0 +1,12 @@
+function traslation(id) {
+    let element = document.getElementById("text-info")
+    if (id == 0) {
+        element.innerHTML = "Hi, i'm Polar4ik(Polarchik) and i'm idiot,\ni made a strange things like this\nyou can check my <a href='https://github.com/Polar4ik'>github</a> or <a href='https://polar4ik.itch.io/'>itch.io</a> to look them"
+    }
+    if (id == 1) {
+        element.innerHTML = 'Привет, я Polar4ik(Полярчик) и я идиот,\nя делаю разные вещи по типу этого\nвы можите посмотреть мой <a href="https://github.com/Polar4ik">github</a> или <a href="https://polar4ik.itch.io/">itch.io</a> что бы посмотреть на них'
+    }
+    if (id == 2) {
+        element.innerHTML = 'Ahoj, já jsem Polar4ik(Polarčik) a já idiot,\n jsem udělám podivné věci jako je tento\nvy můžete se podívat na můj <a href="https://github.com/Polar4ik">github</a> nebo <a href="https://polar4ik.itch.io/">itch.io</a> se na ně podívat'
+    }
+}
