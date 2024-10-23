127e282c2d2aadd0bf1073530ec091b1f6fc2759
Polar4ik <atossnikitoss@gmail.com>
2024-10-23 12:37:32 +0400

    Update text in main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function traslation(id) {
     let element = document.getElementById("text-info")
     if (id == 0) {
-        element.innerHTML = "Hi, i'm Polar4ik(Polarchik) and i'm idiot,\ni made a strange things like this\nyou can check my <a href='https://github.com/Polar4ik'>github</a> or <a href='https://polar4ik.itch.io/'>itch.io</a> to look them"
+        element.innerHTML = "Hi, i'm Polar4ik(Polarchik) and i'm idiot,\ni made a strange things like this\n<a href='https://github.com/Polar4ik'>github</a> <a href='https://polar4ik.itch.io/'>itch.io</a>"
     }
     if (id == 1) {
-        element.innerHTML = 'Привет, я Polar4ik(Полярчик) и я идиот,\nя делаю разные вещи по типу этого\nвы можите посмотреть мой <a href="https://github.com/Polar4ik">github</a> или <a href="https://polar4ik.itch.io/">itch.io</a> что бы посмотреть на них'
+        element.innerHTML = 'Привет, я Polar4ik(Полярчик) и я идиот,\nя делаю разные вещи по типу этого\n<a href="https://github.com/Polar4ik">github</a> <a href="https://polar4ik.itch.io/">itch.io</a>'
     }
     if (id == 2) {
-        element.innerHTML = 'Ahoj, já jsem Polar4ik(Polarčik) a já idiot,\njsem udělám podivné věci jako je tento\nvy můžete se podívat na můj <a href="https://github.com/Polar4ik">github</a> nebo <a href="https://polar4ik.itch.io/">itch.io</a> se na ně podívat'
+        element.innerHTML = 'Ahoj, jsem Polar4ik(Polarčik) a já idiot,\njsem udělám podivné věci jako je tento\<a href="https://github.com/Polar4ik">github</a> <a href="https://polar4ik.itch.io/">itch.io</a>'
     }
 }
 
