465070c1b5263d7a290b049716677dc747aa25ca
Polar4ik <atossnikitoss@gmail.com>
2024-12-11 00:41:57 +0400

    change photoes.html -> photoes.php

--- photoes.php
+++ photoes.php
@@ -10,15 +10,27 @@
     <a class="link", style="top: 16px;", href="./index.html">
         <u>i</u>
     </a>
-    
-    <div class="centered-div">      
-        <div class="gallery"></div>
 
+    <div class="centered-div">
+	<div class="gallery">
+	    <?php
+		$imgFolder = 'img/';
+		$images = scandir($imgFolder);  
+		
+		foreach ($images as $image) {
+		    if ($image !== '.' && $image !== '..') {
+			echo '<div class="photo"> <img src="' . $imgFolder . $image . '" /> </div>';
+		    }
+		}
+	    ?>
+	</div>
         <div id="overlay">
             <img id="fullImage", src="", alt="Full Image">
         </div>
     </div>
-    
+
     <script src="./photoes.js"></script>
 </body>
 </html>
+
+
