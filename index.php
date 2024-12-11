4ad9f86cf498210b303582d7d83d72dbe7ab9f61
Polar4ik <atossnikitoss@gmail.com>
2024-12-11 13:49:51 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Polar4ik Die</title>
+    <link rel="stylesheet" href="./style.css">
+    <script src="./main.js"></script>    
+</head>
+<body class="orange">
+    
+<a class="link", style="top: 16px;" onclick="traslation(0)">
+    <u>en</u>
+</a>
+
+<a class="link", style="top: 38px;" onclick="traslation(1)">
+    <u>ru</u>
+</a>
+
+<a class="link", style="top: 62px;" onclick="traslation(2)">
+    <u>cz</u>
+</a>
+
+<a class="link", style="top: 124px;", href="./photoes.php">
+    <u>p</u>
+</a>
+
+<a class="link", style="top: 146px;", onclick="selectRandomDoc()">
+    <u>t</u>
+</a>
+
+<div class="centered-div">
+<pre id="text-info", class="text">
+Hi, i'm Polar4ik(Polarchik) and i'm idiot,
+i made a strange things like this.
+<a href="https://github.com/Polar4ik">github</a> <a href="https://polar4ik.itch.io/">itch.io</a> discord: @polar4ik
+</pre>
+</div>
+
+</body>
+</html>
